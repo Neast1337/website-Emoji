7e752104364fa882f4738ce3a37522c2e14cf95f
Neast <95341930+Neast1337@users.noreply.github.com>
2022-11-11 14:11:33 +0100

    upload website
    upload website

---
+++ app.js
@@ -0,0 +1,39 @@
+const containerSlot = document.querySelector(".slot");
+const btnConfettis = document.querySelector(".btn-confettis");
+const emojis = ["🌌"];
+
+btnConfettis.addEventListener("click", fiesta);
+
+function fiesta() {
+  
+  if(isTweening()) return;
+
+  for (let i = 0; i < 100; i++) {
+    const confetti = document.createElement("div");
+    confetti.innerText = emojis[Math.floor(Math.random() * emojis.length)];
+    containerSlot.appendChild(confetti);
+  }
+
+  animateConfettis();
+}
+
+function animateConfettis() {
+  
+  const TLCONF = gsap.timeline();
+
+  TLCONF.to(".slot div", {
+    y: "random(-100,100)",
+    x: "random(-100,100)",
+    z: "random(0,1000)",
+    rotation: "random(-90,90)",
+    duration: 1,
+  })
+    .to(".slot div", { autoAlpha: 0, duration: 0.3 }, "-=0.2")
+    .add(() => {
+      containerSlot.innerHTML = "";
+    });
+}
+
+function isTweening(){
+  return gsap.isTweening('.slot div');
+}
